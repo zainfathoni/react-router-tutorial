23c2ca876af86bb6165658eedd62e034181d1e7b
Zain Fathoni <me@zainf.dev>
2023-04-15 21:11:36 +0800

    feat(errors): handling not found errors
    https://reactrouter.com/en/main/start/tutorial#handling-not-found-errors

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./routes/root";
 import "./index.css";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
