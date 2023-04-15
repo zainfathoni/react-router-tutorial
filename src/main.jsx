c20de420e93008f8221dcf93d5d8d9179c757aa4
Zain Fathoni <me@zainf.dev>
2023-04-15 22:57:55 +0800

    feat(loaders): url params in loaders
    https://reactrouter.com/en/main/start/tutorial#url-params-in-loaders

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Root, {
 } from "./routes/root";
 import "./index.css";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
