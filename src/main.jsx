99fc16690dfb412750c53b6f842882f2ad37f7fb
Zain Fathoni <me@zainf.dev>
2023-04-15 22:54:37 +0800

    feat(form): creating contacts
    https://reactrouter.com/en/main/start/tutorial#creating-contacts

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 import "./index.css";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
@@ -12,6 +15,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
