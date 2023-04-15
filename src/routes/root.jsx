ebf56e19b140f45e9f076cc37e2425df0862e75f
Zain Fathoni <me@zainf.dev>
2023-04-15 23:30:28 +0800

    feat(search): synchronizing URL to form state
    https://reactrouter.com/en/main/start/tutorial#synchronizing-urls-to-form-state

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,12 +7,13 @@ import {
   useNavigation,
 } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
+import { useEffect } from "react";
 
 export async function loader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 export async function action() {
@@ -21,8 +22,13 @@ export async function action() {
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
+
   return (
     <>
       <div id="sidebar">
@@ -35,6 +41,7 @@ export default function Root() {
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
