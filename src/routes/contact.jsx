d57884176bae971cf0f5aa39f6e39f8836c5c1a9
Zain Fathoni <me@zainf.dev>
2023-04-15 23:44:33 +0800

    feat(errors): not found data
    https://reactrouter.com/en/main/start/tutorial#not-found-data

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -3,6 +3,12 @@ import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
 
