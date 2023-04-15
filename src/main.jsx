3fabba55c07cbe753164f6816447f0f3b5893cb6
Zain Fathoni <me@zainf.dev>
2023-04-15 21:08:28 +0800

    feat(root): the root route
    https://reactrouter.com/en/main/start/tutorial#the-root-route

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./routes/root";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    elemetn: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
