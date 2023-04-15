9f938c296c840f1c012eec03df7429e390f7ea8d
Zain Fathoni <me@zainf.dev>
2023-04-15 23:34:57 +0800

    feat(search): adding search spinner
    https://reactrouter.com/en/main/start/tutorial#adding-search-spinner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -27,6 +27,10 @@ export default function Root() {
   const navigation = useNavigation();
   const submit = useSubmit();
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
   useEffect(() => {
     document.getElementById("q").value = q;
   }, [q]);
@@ -39,6 +43,7 @@ export default function Root() {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? "loading" : ""}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
@@ -48,7 +53,7 @@ export default function Root() {
                 submit(event.currentTarget.form);
               }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           <Form method="post">
